5f4029e3a216246acfe46b6e03385c88de89c3ab
Rafart132 <alejandrobocourt132@gmail.com>
2025-01-07 17:52:34 -0300

    mejorando animacion

--- Scr/Maquina de estados/entities_states/ChaseState.gd
+++ Scr/Maquina de estados/entities_states/ChaseState.gd
@@ -1,47 +1,30 @@
 extends EntytiStateBase
 class_name chase_state
 
-@export var speed: float = 150.0 # Velocidad al perseguir
-
-# Nodo del jugador (asignado al detectarlo)
+@export var speed: float = 100.0 # Velocidad al perseguir
 var player: Node2D = null
 
 
 func start() -> void:
-	if player == null:
-		print("ChaseState: No se asignó un jugador.")
-	else:
-		print("ChaseState: Iniciando persecución.")
+	pass
 
 func on_process(delta: float) -> void:
 	if player == null:
 		return # No hay jugador que perseguir
 	
 	# Dirección hacia el jugador
-	var direction = (player.position - controlled_node.position).normalized()
-	
+	var direction = (player.position - Maid.position).normalized()
+	Maid.velocity = direction * speed
 	# Movimiento hacia el jugador
-	controlled_node.position += direction * speed * delta
-	
-	# Actualizar la animación según la dirección
-	_update_animation(direction)
-
-func _update_animation(direction: Vector2) -> void:
-	if direction.x > 0:
-		Maid.anim.play("Walk_right")
-	elif direction.x < 0:
-		Maid.anim.play("Walk_left")
-	elif direction.y > 0:
-		Maid.anim.play("Walk_down")
-	elif direction.y < 0:
-		Maid.anim.play("Walk_up")
+	Maid.position += direction * speed * delta
+	Maid.move_and_animate(delta)
 
 func end() -> void:
-	Maid.anim.stop() # Detiene la animación al salir del estado
+	Maid.velocity = Vector2.ZERO
+	Maid.move_and_animate(0)
 
 
 func _on_zona_deteccion_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		print("Jugador detectado. Cambiando al estado de persecución.")
 		player = null
 		state_machine.change_to("PatrolState")
--- escenas/entidades/maid.gd
+++ escenas/entidades/maid.gd
@@ -1,7 +1,26 @@
 extends CharacterBody2D
 class_name maid
 
-const SPEED = 300.0
-
 @onready var anim:AnimatedSprite2D = $AnimatedSprite2D
 @onready var Detect:Area2D = $zona_deteccion
+
+func _process(delta: float) -> void:
+	move_and_animate(delta)
+
+func move_and_animate(_delta: float) -> void:
+	if velocity.length() > 0:
+		move_and_slide()
+		_update_animation()
+	else:
+		anim.stop()
+
+func _update_animation() -> void:
+	if velocity.x > 0:
+		anim.play("Walk_right")
+	elif velocity.x < 0:
+		anim.play("Walk_left")
+
+	elif velocity.y > 0:
+		anim.play("Walk_down")
+	elif velocity.y < 0:
+		anim.play("Walk_up")
